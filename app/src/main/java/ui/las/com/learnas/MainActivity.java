0d37d47e800fecaaa371f1b47c7c2fd2c473b053
xiasang920 <xiasang920@yeah.net>
2016-07-19 10:10:02 +0800

    添加注释

--- app/src/main/java/ui/las/com/learnas/MainActivity.java
+++ app/src/main/java/ui/las/com/learnas/MainActivity.java
@@ -3,6 +3,10 @@ package ui.las.com.learnas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这是程序主入口
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
